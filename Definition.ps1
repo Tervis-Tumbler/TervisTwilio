b8d725329464852acc949be1a58275c62412101c
fhoseinih <hosseini@tervis.com>
2018-05-15 10:24:11 -0400

    Added Get-EmergencyAddress,Add-TervisTwilioAddress,Set-TervisSipTrunkOnPhoneNumber,Get-EmergencyResponseLocationAddresses

---
+++ Definition.ps1
@@ -0,0 +1,621 @@
+﻿$EmergencyResponseAddresses = [PSCustomObject][Ordered]@{
+    Name = "Tervis"
+    Address1 = "201 Triple Diamond Blvd"
+    City = "VEN"
+    ZipCode = "34275"
+    State = "FL"
+    Country = "US"
+},
+[PSCustomObject][Ordered]@{
+    Name = "Tervis Osprey"
+    Address1 = "928 S Tamiami Trail"
+    City = "OSPREY"
+    ZipCode = "34229"
+    State = "FL"
+    Country = "US"
+}
+
+$EmergencyLocations = [PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414701"
+    Name = "Room 161"
+    FriendlyName = "Tervis Tumbler - Room 161"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414702"
+    FriendlyName = "Tervis Tumbler - Room 162"
+    Name = "Room 162"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414700"
+    FriendlyName = "Tervis Tumbler - Osprey Store"
+    Name = "Osprey"
+    AddressName = "Osprey Store"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414703"
+    FriendlyName = "Tervis Tumbler - Room 163"
+    Name = "Room 163"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414704"
+    FriendlyName = "Tervis Tumbler - Room 164"
+    Name = "Room 164"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414705"
+    FriendlyName = "Tervis Tumbler - Room 165"
+    Name = "Room 165"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414706"
+    FriendlyName = "Tervis Tumbler - Room 166"
+    Name = "Room 166"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414707"
+    FriendlyName = "Tervis Tumbler - Room 176"
+    Name = "Room 176"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414708"
+    FriendlyName = "Tervis Tumbler - Room 174"
+    Name = "Room 174"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414709"
+    FriendlyName = "Tervis Tumbler - Room 172"
+    Name = "Room 172"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414710"
+    FriendlyName = "Tervis Tumbler - Room 169"
+    Name = "Room 169"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414711"
+    FriendlyName = "Tervis Tumbler - Room 155"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414712"
+    FriendlyName = "Tervis Tumbler - Room 156"
+    Name = "Room 156"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414713"
+    FriendlyName = "Tervis Tumbler - Room 157"
+    Name = "Room 157"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414714"
+    FriendlyName = "Tervis Tumbler - Room 152"
+    Name = "Room 152"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414715"
+    FriendlyName = "Tervis Tumbler - Room 149"
+    Name = "Room 149"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414716"
+    FriendlyName = "Tervis Tumbler - Room 147"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414717"
+    FriendlyName = "Tervis Tumbler - Room 135"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414718"
+    FriendlyName = "Tervis Tumbler - Room 134"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414719"
+    FriendlyName = "Tervis Tumbler - Room 136"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414720"
+    FriendlyName = "Tervis Tumbler - Room 1137"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414721"
+    FriendlyName = "Tervis Tumbler - Room 138"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414722"
+    FriendlyName = "Tervis Tumbler - Room 139"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414723"
+    FriendlyName = "Tervis Tumbler - Room 131"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414724"
+    FriendlyName = "Tervis Tumbler - Room 144"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414725"
+    FriendlyName = "Tervis Tumbler - Room 113"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414726"
+    FriendlyName = "Tervis Tumbler - Room 146"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414727"
+    FriendlyName = "Tervis Tumbler - Room 148"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414728"
+    FriendlyName = "Tervis Tumbler - Room 116"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414729"
+    FriendlyName = "Tervis Tumbler - Room 111"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414730"
+    FriendlyName = "Tervis Tumbler - Room 112"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414731"
+    FriendlyName = "Tervis Tumbler - Room 110"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+194144147312"
+    FriendlyName = "Tervis Tumbler - Room 108"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414733"
+    FriendlyName = "Tervis Tumbler - Room 145"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414734"
+    FriendlyName = "Tervis Tumbler - Room 129"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414735"
+    FriendlyName = "Tervis Tumbler - Room 130"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414736"
+    FriendlyName = "Tervis Tumbler - Room 127"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414737"
+    FriendlyName = "Tervis Tumbler - Room 125"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414738"
+    FriendlyName = "Tervis Tumbler - Room 126"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414739"
+    FriendlyName = "Tervis Tumbler - Room 124"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414740"
+    FriendlyName = "Tervis Tumbler - Room 103"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414741"
+    FriendlyName = "Tervis Tumbler - Room 102"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414742"
+    FriendlyName = "Tervis Tumbler - Room 101"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414743"
+    FriendlyName = "Tervis Tumbler - Room 104"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414744"
+    FriendlyName = "Tervis Tumbler - Room 100"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414745"
+    FriendlyName = "Tervis Tumbler - Room 122"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414746"
+    FriendlyName = "Tervis Tumbler - Room 118"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414747"
+    FriendlyName = "Tervis Tumbler - Room 142"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414748"
+    FriendlyName = "Tervis Tumbler - Room 143 "
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414749"
+    FriendlyName = "Tervis Tumbler - Area PF-F7"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414750"
+    FriendlyName = "Tervis Tumbler - Area PF-J7"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414751"
+    FriendlyName = "Tervis Tumbler - Area PF-M10"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414752"
+    FriendlyName = "Tervis Tumbler - Area PF-07"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414753"
+    FriendlyName = "Tervis Tumbler - Area PF-Q8"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414754"
+    FriendlyName = "Tervis Tumbler - Room 175"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414756"
+    FriendlyName = "Tervis Tumbler - Bldg Osprey Store"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414757"
+    FriendlyName = "Tervis Tumbler - Room 173"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414758"
+    FriendlyName = "Tervis Tumbler - Room 100A"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414759"
+    FriendlyName = "Tervis Tumbler - Room 100B"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414760"
+    FriendlyName = "Tervis Tumbler - Zone 1"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414761"
+    FriendlyName = "Tervis Tumbler - Zone 2"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414762"
+    FriendlyName = "Tervis Tumbler - Zone 3"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414763"
+    FriendlyName = "Tervis Tumbler - Zone 4"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414764"
+    FriendlyName = "Tervis Tumbler - Zone 5"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414765"
+    FriendlyName = "Tervis Tumbler - Zone 6"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414766"
+    FriendlyName = "Tervis Tumbler - Zone 7"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414767"
+    FriendlyName = "Tervis Tumbler - Zone 8"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414768"
+    FriendlyName = "Tervis Tumbler - Zone 9"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414769"
+    FriendlyName = "Tervis Tumbler - Zone 10"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414770"
+    FriendlyName = "Tervis Tumbler - Zone 11"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414771"
+    FriendlyName = "Tervis Tumbler - Zone 12"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414772"
+    FriendlyName = "Tervis Tumbler - Zone 13"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414773"
+    FriendlyName = "Tervis Tumbler - Zone 14"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414774"
+    FriendlyName = "Tervis Tumbler - Zone 15"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414775"
+    FriendlyName = "Tervis Tumbler - Zone 16"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414776"
+    FriendlyName = "Tervis Tumbler - Zone 17"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414777"
+    FriendlyName = "Tervis Tumbler - Zone 18"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414778"
+    FriendlyName = "Tervis Tumbler - Zone 19"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414779"
+    FriendlyName = "Tervis Tumbler - Zone 20"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414780"
+    FriendlyName = "Tervis Tumbler - Room 200"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414781"
+    FriendlyName = "Tervis Tumbler - Room 165 EE"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414782"
+    FriendlyName = "Tervis Tumbler - Roon 165 EH"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414783"
+    FriendlyName = "Tervis Tumbler - Room 144 A"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414784"
+    FriendlyName = "Tervis Tumbler - Room 144 B"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414785"
+    FriendlyName = "Tervis Tumbler - Room 145 A"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414786"
+    FriendlyName = "Tervis Tumbler - Room 145 B"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414787"
+    FriendlyName = "Tervis Tumbler - Room 140"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414788"
+    FriendlyName = "Tervis Tumbler - Room 102 A IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414789"
+    FriendlyName = "Tervis Tumbler - Room 102 B IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414790"
+    FriendlyName = "Tervis Tumbler - Room 103"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414791"
+    FriendlyName = "Tervis Tumbler - Room 106 IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414792"
+    FriendlyName = "Tervis Tumbler - Room 107 IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414793"
+    FriendlyName = "Tervis Tumbler - Room 108 IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414794"
+    FriendlyName = "Tervis Tumbler - Room 114 IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414795"
+    FriendlyName = "Tervis Tumbler - Room 119 IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414796"
+    FriendlyName = "Tervis Tumbler - Room 121 IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414797"
+    FriendlyName = "Tervis Tumbler - Room 104 IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414798"
+    FriendlyName = "Tervis Tumbler - Room 202 IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414799"
+    FriendlyName = "Tervis Tumbler - Room 205 IVC"
+    Name = "Room 155"
+    AddressName = "Tervis"
+},
+[PSCustomObject][Ordered]@{
+    DirectNumber = "+19414414700"
+    FriendlyName = "Tervis Tumbler - Front Lobby"
+    Name = "Room 155"
+    AddressName = "Tervis"
+}
+
+
+
+
--- TervisTwilio.psm1
+++ TervisTwilio.psm1
@@ -1,3 +1,72 @@
-﻿Set-GetTwilioCredentialScriptBlock -ScriptBlock {
-    Get-PasswordstatePassword -AsCredential -ID 5313
+﻿$ModulePath = (Get-Module -ListAvailable TervisTwilio).ModuleBase
+. $ModulePath\Definition.ps1
+
+Set-GetTwilioCredentialScriptBlock -ScriptBlock {
+    Get-PasswordstateCredential -PasswordID 5313
 }
+
+function Get-EmergencyAddress {
+    param (
+        $Name
+    )
+    $EmergencyResponseAddresses |
+    Where-Object Name -EQ $Name
+}
+
+function Get-EmergencyResponseLocationAddresses {
+    foreach ($EmergencyLocation in $EmergencyLocations) {
+        $EmergencyAddress = Get-EmergencyAddress -Name $EmergencyLocation.AddressName
+        $EmergencyAddress |
+        Add-Member -MemberType NoteProperty -Name Address2 -Value $EmergencyLocation.Name -Force -PassThru |
+        Add-Member -MemberType NoteProperty -Name DirectNumber -Value $EmergencyLocation.DirectNumber -Force -PassThru |
+        Add-Member -MemberType NoteProperty -Name FriendlyName -Value $EmergencyLocation.FriendlyName -Force -PassThru      
+    }
+}
+
+function Add-TervisTwilioAddress {
+    param (
+        [Parameter(Mandatory,ValueFromPipeLinebyPropertyName)][String]$FriendlyName,
+        [Parameter(Mandatory,ValueFromPipeLinebyPropertyName)][String]$CustomerName,
+        [Parameter(Mandatory,ValueFromPipeLinebyPropertyName)][String]$Street,
+        [Parameter(Mandatory,ValueFromPipeLinebyPropertyName)][String]$City,
+        [Parameter(Mandatory,ValueFromPipeLinebyPropertyName)][String]$Region,
+        [Parameter(Mandatory,ValueFromPipeLinebyPropertyName)][String]$PostalCode,
+        [Parameter(Mandatory,ValueFromPipeLinebyPropertyName)][String]$IsoCountry
+     )
+     process {
+        $ExistingFriendlyName = Get-TwilioAddresses | select -ExpandProperty addresses | select -ExpandProperty friendly_name
+        if ($ExistingFriendlyName -eq $FriendlyName) {
+        (Write-Host "The address with the same Friendly_Name already existed")
+        } else {
+    Add-TwilioAddresses -FriendlyName $FriendlyName -CustomerName $CustomerName -Street $Street -City $City -Region $Region -PostalCode $PostalCode -IsoCountry $IsoCountry
+          }
+    }
+}
+
+function Set-TervisSipTrunkOnPhoneNumber {
+    param (
+       [Parameter(Mandatory,ValueFromPipeLine)][string]$FriendlyName,
+       $TrunkSid,
+       $ApiVersion,
+       $VoiceUrl,
+       $VoiceMethod,
+       $VoiceFallbackUrl,
+       $VoiceFallbackMethod,
+       $StatusCallback,
+       $StatusCallbackMethod,
+       $VoiceCallerIdLookup,
+       $VoiceApplicationSid,
+       $SmsUrl,
+       $SmsMethod,
+       $SmsFallbackUrl,
+       $SmsFallbackMethod,
+       $SmsApplicationSid,
+       $AccountSid, 
+       $AddressSid
+    
+    )
+    process {
+        Set-TwilioIncomingPhoneNumber -FriendlyName $FriendlyName -TrunkSid "TK59ae204b72e3b329cd361c0060c4e17c"
+    }
+}
+
